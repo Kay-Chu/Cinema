fae4872f0cac2a2275f39604587e4a128567ab63
Kaying <39971215+Kay-Chu@users.noreply.github.com>
2023-09-09 22:15:55 +0800

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: "https://demo.code.chu.ljc42.com/api/"
-    baseURL: "http://localhost:3001/api"
+    baseURL: "http://localhost:3000/api"
 });
